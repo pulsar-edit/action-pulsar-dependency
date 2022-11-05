5fc8daf82f3cadb97b251cb14bb125f709d09937
confused-Techie <dev@lhbasics.com>
2022-11-05 14:29:55 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const shell = require("shelljs");
+const path = require("path");
+const fs = require("fs");
+
+try {
+
+  // First lets move the current repo's code into a subfolder
+
+  await copyDir("./", "./package");
+
+
+  // Then we need to clone the current Pulsar Repo
+  shell.exec("git clone https://github.com/pulsar-edit/pulsar");
+
+  // Now we need the package we are testing.
+  const pack = core.getInput('package-to-test');
+
+  if (pack === '') {
+    core.setFailed('No Package has been specified to test!');
+  }
+
+  // Now with the package we know we are testing, lets update the `package.json`
+  // to point to the local copy of this package.
+
+  let packJSON = require("./pulsar/package.json");
+  packJSON.dependencies[pack] = `./${pack}`;
+
+  // Now with the package.json poiting to our local dependencies, we will save the file
+  fs.writeFileSync("./pulsar/package.json", JSON.stringify(packJSON, null, 2));
+
+  // Then lets run our commands to install, build, and run test
+  shell.exec("yarn install");
+  shell.exec("yarn build");
+  shell.exec("yarn start --test spec");
+
+
+} catch(err) {
+  core.setFailed(err.message);
+}
+
+/**
+  * Thanks @KyleMit
+  * @see {@link https://stackoverflow.com/a/64255382/12707685}
+  */
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
+  let entries = await fs.readdir(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    let srcPath = path.join(src, entry.name);
+    let destPath = path.join(dest, entry.name);
+
+    entry.isDirectory() ?
+      await copyDir(srcPath, destPath) :
+      await fs.copyFile(srcPath, destPath);
+  }
+}
